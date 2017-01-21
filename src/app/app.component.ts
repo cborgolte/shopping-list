881c65316bbc4e59b36ddfad52f32a93b315edb0
Christoph Borgolte <christoph.borgolte@gmail.com>
2017-01-21 14:45:51 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+export class LineItem {
+  id: number;
+  qty: number;
+  // unit: string;
+  name: string;
+  selected: boolean;
+  // bought: boolean;
+  // categories: Array<string>;
+}
+
+const LINE_ITEMS : LineItem[] = [
+  {
+    id: 1,
+    qty: 1,
+    name: "Kartoffeln",
+    selected: false
+  },
+  {
+    id: 2,
+    qty: 5,
+    name: "Zwiebeln",
+    selected: true
+  }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Shopping List';
+  lineItems = LINE_ITEMS;
+  lineItem = new LineItem();
 }
