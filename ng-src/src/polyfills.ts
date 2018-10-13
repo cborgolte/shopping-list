ab4e7b7fe7e43f6e93defb6477d01160ff843757
Christoph Borgolte <christoph.borgolte@gmail.com>
2018-10-13 15:43:44 +0200

    Fix: Add polyfill for 'process'

--- ng-src/src/polyfills.ts
+++ ng-src/src/polyfills.ts
@@ -79,6 +79,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 
+import * as process from 'process';
 
 (window as any).global = window;
-
+window['process'] = process;
