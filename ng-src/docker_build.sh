f370af1df7c276a2f190c147d370339c5144fef6
Christoph Borgolte <christoph.borgolte@gmail.com>
2019-02-22 23:06:23 +0100

    Tag built images w/ git commit hash

--- ng-src/docker_build.sh
+++ ng-src/docker_build.sh
@@ -2,8 +2,13 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+GIT_REV=$(git log --pretty=format:'%h' -n 1)
+IMAGE_NAME=cborgolte/shopping-list
+
+echo "building $IMAGE_NAME:$GIT_REV"
 ng build --prod --aot --output-path=docker/dist
 cd docker
-docker build -t cborgolte/shopping-list .
-docker push cborgolte/shopping-list:latest
+docker build -t $IMAGE_NAME:latest -t $IMAGE_NAME:$GIT_REV .
+docker push $IMAGE_NAME:latest
+docker push $IMAGE_NAME:$GIT_REV
 
